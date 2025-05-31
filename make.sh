bc545d68702ca01f912c85ad8cb516d0834fd819
Jack Faller <jack.t.faller@gmail.com>
2025-05-31 02:46:28 +0100

    Adds release builds

--- make.sh
+++ make.sh
@@ -2,21 +2,50 @@
 set -exo pipefail
 
 cd "$(dirname "$(which $0)")"
+SELF="$(basename "$0")"
 
 NAME="$(grep '^project(.*)$' CMakeLists.txt | sed 's/^project(\(.*\))$/\1/')"
 while [ $# != 0 ]; do
 	case "$1" in
 		web)
-			docker run --rm --network=host --interactive --tty --volume "$(pwd):/proj" --workdir /proj --user $UID:$GID emscripten/emsdk sh -c 'emcmake cmake -S . -B emscripten-build && cd emscripten-build && emmake make --jobs "$(nproc)"'
+			RELEASE="--debug"
+			if [ "$2" = --release ]; then
+				shift
+				RELEASE="--release"
+			fi
+			docker run --rm --network=host --interactive --tty --volume "$(pwd):/proj" --user $UID:$GID emscripten/emsdk \
+				   "/proj/$SELF" generic emscripten "$RELEASE"
 			;;
 		glad)
 			glad --api='gles2=3.0' --out-path=libraries/glad
 			;;
 		linux)
-			if ! [ -e linxu-build ]; then
-				cmake -S . -B linux-build
+			RELEASE="--debug"
+			if [ "$2" = --release ]; then
+				shift
+				RELEASE="--release"
 			fi
-			cmake --build linux-build --parallel "$(nproc)"
+			./"$SELF" generic linux "$RELEASE"
+			;;
+		generic)
+			TARGET="$2"
+			TYPE="$3"
+			shift 2
+			RELEASE=-DCMAKE_BUILD_TYPE=Debug
+			DIR="$TARGET"-build
+			if [ "$TYPE" = --release ]; then
+				shift
+				RELEASE=-DCMAKE_BUILD_TYPE=Release
+				DIR="$TARGET"-release
+			fi
+			if ! [ -e "$DIR" ]; then
+				if [ "$TARGET" = emscripten ]; then
+					emcmake cmake "$RELEASE" -S . -B "$DIR"
+				else
+					cmake "$RELEASE" -S . -B "$DIR"
+				fi
+			fi
+			cmake --build "$DIR" --parallel "$(nproc)"
 			;;
 		run)
 			shift
