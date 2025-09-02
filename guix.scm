4dbb572a5e95fe1cd74472558fd1ccdb4abbf610
Jack Faller <jack.t.faller@gmail.com>
2025-09-02 19:19:47 +0100

    Fixes some errors with path names

--- guix.scm
+++ guix.scm
@@ -1,22 +1,22 @@
 (use-modules (guix gexp)
-			 (guix packages)
-			 (guix git-download)
+						 (guix packages)
+						 (guix git-download)
              (guix build-system cmake)
-			 (nongnu packages nvidia)
-			 ((guix licenses) #:prefix license:)
-			 (gnu packages))
+						 (nongnu packages nvidia)
+						 ((guix licenses) #:prefix license:)
+						 (gnu packages))
 
-(define sd3-test
+(define sd3-template
   (package
-   (name "sd3-test")
+   (name "sd3-template")
    (version "0.0.0")
    (source (local-file (dirname (current-filename)) #:recursive? #t))
    (build-system cmake-build-system)
    (native-inputs (map specification->package '("glad")))
-   (inputs (cons (replace-mesa (specification->package "sdl3"))
-				 (map specification->package '())))
+   (inputs (cons (specification->package "sdl3")
+								 (map specification->package '())))
    (home-page "")
    (synopsis "")
    (description "")
    (license license:gpl3)))
-sd3-test
+sd3-template
--- make.sh
+++ make.sh
@@ -38,7 +38,7 @@ while [ $# != 0 ]; do
 			TYPE="$3"
 			shift 2
 			RELEASE=-DCMAKE_BUILD_TYPE=Debug
-			DIR="$TARGET"-build
+			DIR="$TARGET"-debug
 			if [ "$TYPE" = --release ]; then
 				RELEASE=-DCMAKE_BUILD_TYPE=Release
 				DIR="$TARGET"-release
@@ -55,7 +55,12 @@ while [ $# != 0 ]; do
 			;;
 		run)
 			shift
-			exec "./linux-build/$NAME" "$@"
+			if [ "$1" = --release ]; then
+				shift
+				exec "./linux-debug/Release/$NAME" "$@"
+			else
+				exec "./linux-debug/Debug/$NAME" "$@"
+			fi
 			;;
 		pack)
 			DIR=emscripten-build/Debug
@@ -63,11 +68,11 @@ while [ $# != 0 ]; do
 				shift
 				DIR=emscripten-release/Release
 			fi
-			gcc tools/base128-encode.c -o emscripten-build/base128-encode
+			gcc tools/base128-encode.c -o emscripten-debug/base128-encode
 			cp "src/shell.html" "$DIR/$NAME-pack.html"
 			add_pack_file() {
 				(echo -n "	{ name: '${3-$2}', type: '$1', text: '"
-				 gzip --to-stdout "$DIR/$2" | ./emscripten-build/base128-encode
+				 gzip --to-stdout "$DIR/$2" | ./emscripten-debug/base128-encode
 				 echo "' },"
 				) | sed '/!! DATA_STRINGS !!/r /dev/stdin' -i "$DIR/$NAME-pack.html"
 			}
@@ -75,6 +80,10 @@ while [ $# != 0 ]; do
 			add_pack_file application/octet-stream "$NAME.data"
 			add_pack_file application/javascript "$NAME.js" "script.js"
 			sed '/{{{ SCRIPT }}}/d' -i "$DIR/$NAME-pack.html"
+			;;
+		*)
+			echo "Unknown target \"$1\""
+			exit 1
 			;;
 	esac
 	shift
